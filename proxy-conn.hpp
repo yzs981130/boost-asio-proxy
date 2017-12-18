50e19fce1663b74ba3e58ce63d5f0031f05d4c6b
Tizen Ye <yzs981130@126.com>
2017-12-18 07:58:57 -0800

    check communication to dns server

--- proxy-conn.hpp
+++ proxy-conn.hpp
@@ -16,113 +16,117 @@
 
 class connection : public boost::enable_shared_from_this<connection> {
 public:
-	typedef boost::shared_ptr<connection> pointer;
+    typedef boost::shared_ptr<connection> pointer;
 
-	static pointer create(ba::io_service& io_service) {
-		return pointer(new connection(io_service));
-	}
+    static pointer create(ba::io_service &io_service) {
+        return pointer(new connection(io_service));
+    }
 
-	ba::ip::tcp::socket& socket() {
-		return bsocket_;
-	}
+    ba::ip::tcp::socket &socket() {
+        return bsocket_;
+    }
 
-	/// Start read data of request from browser
-	void start();
+    /// Start read data of request from browser
+    void start();
 
 private:
-	connection(ba::io_service& io_service);
+    connection(ba::io_service &io_service);
 
-	/// Read header of HTTP request from browser
-	void handle_browser_read_headers(const bs::error_code& err, size_t len);
-	
-	/// Start connecting to the web-server, initially to resolve the DNS-name of Web server into the IP address
-	void start_connect();
+    /// Read header of HTTP request from browser
+    void handle_browser_read_headers(const bs::error_code &err, size_t len);
 
-  std::string query_name(const std::string &qname);
+    /// Start connecting to the web-server, initially to resolve the DNS-name of Web server into the IP address
+    void start_connect();
 
-  void handle_dns_connect(const bs::error_code &err, size_t len);
+    std::string query_name(const std::string &qname);
 
-  void handle_dns_read_answer(const bs::error_code &err, size_t len);
-	void handle_resolve(const boost::system::error_code& err,
-									ba::ip::tcp::resolver::iterator endpoint_iterator);
-	void handle_connect(const boost::system::error_code& err,
-									ba::ip::tcp::resolver::iterator endpoint_iterator, const bool first_time);
+    void handle_dns_connect(const bs::error_code &err, size_t len);
 
-	/// Write data to the web-server
-	void start_write_to_server();
-	void handle_server_write(const bs::error_code& err, size_t len);
+    void handle_dns_read_answer(const bs::error_code &err, size_t len);
 
-	/// Read header of data returned from the web-server
-	void handle_server_read_headers(const bs::error_code& err, size_t len);
+    void handle_resolve(const boost::system::error_code &err,
+                        ba::ip::tcp::resolver::iterator endpoint_iterator);
 
-	/// Reading data from a Web server, and writing it to the browser
-	void handle_browser_write(const bs::error_code& err, size_t len);
-	void handle_server_read_body(const bs::error_code& err, size_t len);
+    void handle_connect(const boost::system::error_code &err,
+                        ba::ip::tcp::resolver::iterator endpoint_iterator, const bool first_time);
 
-	/// Close both sockets: for browser and web-server
-	void shutdown();
+    /// Write data to the web-server
+    void start_write_to_server();
 
-	ba::io_service& io_service_;
-	ba::ip::tcp::socket bsocket_;
-	ba::ip::tcp::socket ssocket_;
-  ba::ip::udp::socket dns_socket_;
-	ba::ip::tcp::resolver resolver_;
-	bool proxy_closed;
-	bool isPersistent;
-	int32_t RespLen;
-	int32_t RespReaded;
+    void handle_server_write(const bs::error_code &err, size_t len);
 
-	boost::array<char, 8192> bbuffer;
-	boost::array<char, 8192> sbuffer;
+    /// Read header of data returned from the web-server
+    void handle_server_read_headers(const bs::error_code &err, size_t len);
 
-	std::string fURL;
-	std::string fHeaders;
-	std::string fNewURL;
-	std::string fMethod;
-	std::string fReqVersion;
-	std::string fServer;
-	std::string fPort;
-	bool isOpened;
+    /// Reading data from a Web server, and writing it to the browser
+    void handle_browser_write(const bs::error_code &err, size_t len);
 
-	std::string fReq;
+    void handle_server_read_body(const bs::error_code &err, size_t len);
 
-  // check if this request is about videos
-  void check_video_requests(const std::string &uri);
+    /// Close both sockets: for browser and web-server
+    void shutdown();
 
-  bool isVideoMeta;
-  std::string pathToVideo;
-  std::string metafileName;
-  bool isBigBuckBunny;
+    ba::io_service &io_service_;
+    ba::ip::tcp::socket bsocket_;
+    ba::ip::tcp::socket ssocket_;
 
-  std::vector<int32_t> get_bitrates(const std::string &xml);
+    ba::ip::tcp::resolver resolver_;
+    bool proxy_closed;
+    bool isPersistent;
+    int32_t RespLen;
+    int32_t RespReaded;
 
-  bool isVideoChunk;
-  std::string segNum;
-  std::string fragNum;
+    boost::array<char, 8192> bbuffer;
+    boost::array<char, 8192> sbuffer;
 
-  std::string adapt_bitrate(const ba::ip::address_v4 &ip, const std::string &path,
-                            const std::string &seg, const std::string &frag);
+    std::string fURL;
+    std::string fHeaders;
+    std::string fNewURL;
+    std::string fMethod;
+    std::string fReqVersion;
+    std::string fServer;
+    std::string fPort;
+    bool isOpened;
 
-	typedef boost::unordered_map<std::string,std::string> headersMap;
-	headersMap reqHeaders, respHeaders;
+    std::string fReq;
 
-	void parseHeaders(const std::string& h, headersMap& hm);
+    // check if this request is about videos
+    void check_video_requests(const std::string &uri);
 
-	// recored the send and receive time
-  bc::steady_clock::time_point tStart;
+    bool isVideoMeta;
+    std::string pathToVideo;
+    std::string metafileName;
+    bool isBigBuckBunny;
 
-	/// Record / Update throughput from proxy to servers
-  void update_throughput(const int32_t &size,
-                         const bc::steady_clock::time_point &timeStart,
-                         const ba::ip::address_v4 &ip);
+    std::vector<int32_t> get_bitrates(const std::string &xml);
 
-  static boost::unordered_map<ba::ip::address_v4, std::pair<double, std::vector<int32_t>>> throughputMap;
-  static boost::shared_mutex tm_mutex;
-  static double update_alpha;
+    bool isVideoChunk;
+    std::string segNum;
+    std::string fragNum;
 
-  static std::string wwwip;
-  static ba::ip::address_v4 dns_ip;
-  static unsigned short dns_port;
+    std::string adapt_bitrate(const std::string &ip, const std::string &path,
+                              const std::string &seg, const std::string &frag);
+
+    typedef boost::unordered_map<std::string, std::string> headersMap;
+    headersMap reqHeaders, respHeaders;
+
+    void parseHeaders(const std::string &h, headersMap &hm);
+
+    // recored the send and receive time
+    bc::steady_clock::time_point tStart;
+
+    /// Record / Update throughput from proxy to servers
+    void update_throughput(const int32_t &size,
+                           const bc::steady_clock::time_point &timeStart,
+                           const std::string &ip);
+
+    static boost::unordered_map<std::string, std::pair<double, std::vector<int32_t>>> throughputMap;
+    static boost::shared_mutex tm_mutex;
+    static double update_alpha;
+
+    static std::string wwwip;
+    static ba::ip::address_v4 dns_ip;
+    static unsigned short dns_port;
 };
 
 #endif /* _PROXY-CONN_H */
--- proxy.cpp
+++ proxy.cpp
@@ -8,43 +8,55 @@
  */
 
 #include "proxy-server.hpp"
+boost::unordered_map<std::string, std::pair<double, std::vector<int32_t>>> connection::throughputMap;
+boost::shared_mutex connection::tm_mutex;
+double connection::update_alpha = 1.0;
+std::string connection::wwwip;
+ba::ip::address_v4 connection::dns_ip = ba::ip::address_v4::from_string("127.0.0.1");
+unsigned short connection::dns_port = 10053;
 
-int main(int argc, char** argv) {
-	try {
-		int thread_num = 2, port = 10001;
-		std::string interface_address;
+int main(int argc, char **argv) {
+    try {
+        int thread_num = 2, port = 10001;
+        std::string interface_address;
 
-		//TODO: reset command-line arguments
-    // read thread numbers form command line, if provided
-		if(argc > 1)
-			thread_num = boost::lexical_cast<int>(argv[1]);
+        //TODO: reset command-line arguments
+        if (argc != 7 && argc != 8) {
+            std::cerr << "Usage: " << argv[0]
+                      << " <log> <alpha> <listen-port> <fake-ip> <dns-ip> <dns-port> [<www-ip>]."
+                      << std::endl;
+            exit(1);
+        }
+        // read thread numbers form command line, if provided
+        if (argc > 1)
+            thread_num = boost::lexical_cast<int>(argv[1]);
 
-    // read port number from command line, if provided
-		if(argc > 2)
-			port = boost::lexical_cast<int>(argv[2]);
+        // read port number from command line, if provided
+        if (argc > 2)
+            port = boost::lexical_cast<int>(argv[2]);
 
-    // read local interface address from command line, if provided
-		if(argc > 3)
-			interface_address = argv[3];
+        // read local interface address from command line, if provided
+        if (argc > 3)
+            interface_address = argv[3];
 
-    ios_deque io_services;
-		std::deque<ba::io_service::work> io_service_work;
-		
-		boost::thread_group thr_grp;
-		
-		for (int i = 0; i < thread_num; ++i) {
-			io_service_ptr ios(new ba::io_service);
-			io_services.push_back(ios);
-			io_service_work.push_back(ba::io_service::work(*ios));
-			thr_grp.create_thread(boost::bind(&ba::io_service::run, ios));
-		}
-		server server(io_services, port, interface_address);
-		thr_grp.join_all();
-	} catch (std::exception& e) {
-		std::cerr << e.what() << std::endl;
-	}
+        ios_deque io_services;
+        std::deque<ba::io_service::work> io_service_work;
+
+        boost::thread_group thr_grp;
+
+        for (int i = 0; i < thread_num; ++i) {
+            io_service_ptr ios(new ba::io_service);
+            io_services.push_back(ios);
+            io_service_work.push_back(ba::io_service::work(*ios));
+            thr_grp.create_thread(boost::bind(&ba::io_service::run, ios));
+        }
+        server server(io_services, port, interface_address);
+        thr_grp.join_all();
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
 
 
-	return 0;
+    return 0;
 }
 
